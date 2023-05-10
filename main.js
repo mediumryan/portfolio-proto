6262c503813e727664e63cd1ddb77610dc4960aa
Replit user <>
2023-05-10 08:31:01 +0000

    start js

---
+++ main.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById('navbar');
+const greeting = document.getElementById('greeting');
+const skills = document.getElementById('skills');
+const projects = document.getElementById('projects');
+const contact = document.getElementById('contact');
+
+console.dir(navbar);
